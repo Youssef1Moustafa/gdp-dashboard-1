3eb79b575232eb3801fdc46aadb503a1d940d569
Youssef Moustafa <109866940+Youssef1Moustafa@users.noreply.github.com>
2024-08-23 18:51:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ f1 = st.number_input('feature_1',  min_value = 1, max_value = 10)
 f2 = st.number_input('feature_2',  min_value = 1, max_value = 10)
 f3 = st.number_input('feature_3',  min_value = 1, max_value = 10)
 
+with open('model.pkl', 'rb') as file:
+  model = pickle.load(file)
+
+model.predict
